f4d704d3e696019ba7c7d66aedfc58325d5d62ec
Jeason Campos <camposjeason@gmail.com>
2023-05-21 19:44:14 -0400

    4.0.0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,8 +2,8 @@
 const Layout = ({ children }) => {
   return (
     <>
-
-      <div>
+      <div className='grid md:grid-cols-2 gap-1 m-1 mr-[5px] sm:mr-1'>
+        {children}
       </div>
     </>
   )
